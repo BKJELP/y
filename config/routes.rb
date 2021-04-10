174030fb4d877f432ee7ce018208983280b6d094
Brice Levasseur <brice.k.levasseur@gmail.com>
2021-04-10 17:26:36 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Créer une tâche
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  
+  # Lister toutes les tâches
+  get "tasks", to: "tasks#index"
+
+  # Lire une seule tâche
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Modifier les infos d'une tâche
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # Supprimer une tâche
+  delete "tasks/:id", to: "tasks#destroy"
 end
